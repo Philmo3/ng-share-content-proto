449c1b298d019d65c99bb52cdc26b9912d894bad
Philippe Brunet <phil_27mo@hotmail.com>
2023-02-19 17:10:17 -0500

    first commit 2

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket'
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionService {
 
+  private webSocketSub?: WebSocketSubject<any>
+
   constructor() { }
+
+  connect(){
+    this.webSocketSub = webSocket('')
+  }
+
 }
